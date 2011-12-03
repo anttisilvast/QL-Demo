0f2bb2216c35a22fd2778f80990df92d14d02e20
Antti Silvast <antti.silvast@gmail.com>
2011-12-03 19:18:44 +0200

    unfinished draft

---
+++ putpixel.s
@@ -0,0 +1,61 @@
+; An unfinished putpixel routine for sinclair QL.
+; To compile, use the vasm assembler as follows: 
+; vasmm68k_mot -m68000 -Fbin -o putpixel.bin putpixel.s
+
+	org	261120
+start:
+	movem	d0-d2/a0-a1,-(a7)
+
+	bsr clearscreen
+
+	move.l #131072,a0
+
+	move.w	#5,d0
+	move.w	#200,d1
+	bsr putpixel
+	move.w	#0,d0
+	move.w	#201,d1	
+	bsr putpixel
+
+; exit
+	movem	(a7)+,d0-d2/a0-a1
+	clr.l	d0
+	rts
+
+putpixel:
+; INPUT: 
+;   a0.l 		screen base address
+;   d0.w 	x coordinate
+;   d1.w 	y coordinate
+; 	d2.b		colour (GFRB, i.e. Green,Flash,Red,Blue)
+	move.l  a0,-(a7) ; push a0
+
+; the screen is organized as planar bytes, as follows:
+; GFGFGFGF RBRBRBRB
+; Hence the address to draw to is a0+d1*128+(d0/4)*2 
+
+ 	lsl.w			#7,d1
+	adda.l		d1,a0 ; a0+=d1*128
+
+	move.w	d0,d2
+	and.w		#$FC,d2
+	asr.w			#1,d2
+	adda.l		d2,a0  ;a0+=2*(d0/4)
+
+	; now determine the mask
+	;add.l			#1,a0
+
+	move.b 	#$FF,d2
+	move.b 	d2,(a0)
+
+	move.l  (a7)+,a0  ; pop a0
+	rts
+ 
+clearscreen:
+	move.l  #131072,a0
+	move.l	#0,d0
+	move.l	#8192,d1
+loop_clear:
+	move.l	d0,(a0)+
+	dbra 	d1,loop_clear
+	rts
