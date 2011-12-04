a4337b9352db443afbb9e30954df4e07d3cb1433
Antti Silvast <antti.silvast@gmail.com>
2011-12-04 16:17:56 +0200

    update

--- putpixel.s
+++ putpixel.s
@@ -6,6 +6,16 @@
 ; make putpixel.bin
 
 	org	261120
+
+; preset colour masks
+RED	equ	$80
+GREEN	equ	$8000
+BLUE	equ	$40
+YELLOW	equ	(RED+GREEN)
+PURPLE	equ	(RED+BLUE)
+CYAN	equ	(GREEN+BLUE)
+WHITE	equ	(RED+GREEN+BLUE)
+
 start:
 	movem	d0-d2/a0-a1,-(a7)
 
@@ -13,12 +23,31 @@ start:
 
 	move.l #131072,a0
 
-	move.w	#5,d0
-	move.w	#200,d1
+	move.w #128,d0
+	move.w #128,d1
+	move.w #RED,d2
 	bsr putpixel
-	move.w	#0,d0
-	move.w	#201,d1	
+	addq #2,d0
+	addq #2,d1
+	move.w #GREEN,d2
 	bsr putpixel
+	addq #2,d0
+	addq #2,d1
+	move.w #BLUE,d2
+	bsr putpixel
+	addq #2,d0
+	addq #2,d1
+	move.w #PURPLE,d2
+	bsr putpixel
+	addq #2,d0
+	addq #2,d1
+	move.w #CYAN,d2
+	bsr putpixel
+	addq #2,d0
+	addq #2,d1
+	move.w #WHITE,d2
+	bsr putpixel
+
 
 ; exit
 	movem	(a7)+,d0-d2/a0-a1
@@ -30,34 +59,40 @@ putpixel:
 ;   a0.l screen base address
 ;   d0.w x coordinate
 ;   d1.w y coordinate
-;   d2.w colour 
+;   d2.w colour (see colour constants in the beginning)
 	
-	move.l  a0,-(a7) ; push a0
+	move.l a0,-(a7)
+	move.l d0,-(a7)
+	move.l d1,-(a7)
+
 ; the screen is organized as planar bytes as follows:
 ; GFGFGFGF RBRBRBRB
 ; (G=green bit,F=flash bit,R=red bit,B=blue bit)
 ; The low-res screen is 256 pixels wide
-; Hence the address to draw to is a0+d1*128+(d0/4)*2 
+; Hence the address to draw to is a0+y*128+(x/4)*2 
 
  	lsl.w #7,d1
 	adda.l d1,a0 ; a0+=d1*128
 
-	move.w d0,d2
-	and.w #$FC,d2
-	asr.w #1,d2
-	adda.l d2,a0  ;a0+=2*(d0/4)
+	move.w d0,d1 ; spare d0 in d1
 
-; Now determine the colour mask and OR it to the screen
-; from here, unfinished
-; but the idea is to have the color
-; value as the mask, which is 
-; then shifted right according
-; to the (x-coordinate & 0x3)
+	and.w #$FC,d0
+	lsr.w #1,d0
+	adda.l d0,a0  ;a0+=2*(d0/4)
 
-	move.b #$FF,d2	; a dummy color for now
-	move.b d2,(a0)
+; Now determine the colour mask and OR it to the screen.
+; With colour (d2) as the initial mask, the pixel is 
+; shifted right by (x & 3)*2 to align with the correct location
 
-	move.l (a7)+,a0  ; pop a0
+	and.w #$03,d1
+	lsl.l #1,d1
+	lsr.w d1,d2
+	or.w d2,(a0)
+
+	move.l (a7)+,d1
+	move.l (a7)+,d0
+	move.l (a7)+,a0
+
 	rts
  
 clearscreen:
